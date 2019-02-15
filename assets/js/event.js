1ed299a04057bd9e2a56df1d5592fb9333f8e989
Matt Coster <matt@tedxwarwick.com>
2019-02-15 05:32:19 +0000

    Fixes for speaker popovers

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,51 +1,56 @@
 'use strict';
 
-//var SpeakerPopover = document.getElementById("{{ speaker.slug }}");
-var currentSpeaker;
-
 x.onPageLoad(() => {
   const cards = document.getElementsByClassName('speakers')[0]
                         .getElementsByClassName('card');
 
   for (let card of cards) {
     card.addEventListener('click', e => {
-      x.presentSpeaker(e.target.dataset.slug);
+      const popoverId = `popover-${e.target.dataset.slug}`;
+      x.presentPopover(document.getElementById(popoverId));
     });
   }
+
+  document.getElementById('overlay').addEventListener('click', () => {
+    x.dismissPopover();
+  });
+
+  document.querySelectorAll('.popover .close').forEach(btn => {
+    btn.addEventListener('click', () => {
+      x.dismissPopover();
+    });
+  });
+
+  window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      x.dismissPopover();
+    }
+  })
 });
 
 x.extend({
-  presentSpeaker: speaker => {
-    console.log('Presenting:', speaker);
-    //background = (body > div:not(#speaker));
+  activePopover: null,
+  presentPopover: popover => {
+    if (!x.activePopover) {
+      x.activePopover = popover;
 
-    // var allElements = document.getElementsById("*");
-    // for (var i = 0, len = allElements.length; i < len; i++) {
-    //   if (allElements[i] != speaker){
-    //     allElements[i].classList.add('blur');
-    //   }
-    // }
-    //
+      popover.querySelectorAll('.ytembed').forEach(vid => {
+        vid.src = vid.dataset.src;
+      });
 
-    //document.getElementById(overlay).classList.add('blur');
-    document.body.classList.add('blur');
-    //console.log(document.body.classList);
-    document.getElementById(speaker).style.display = "block";
-    // console.log(document.getElementById(speaker).classList);
-    // document.getElementById(speaker).classList.remove('blur');
-    // console.log(document.getElementById(speaker).classList);
-    currentSpeaker = speaker;
+      popover.classList.remove('popover-hidden');
+      document.body.classList.add('blur');
+    }
+  },
+  dismissPopover: () => {
+    if (x.activePopover) {
+      x.activePopover.querySelectorAll('.ytembed').forEach(vid => {
+        vid.src = '';
+      });
+
+      document.body.classList.remove('blur');
+      x.activePopover.classList.add('popover-hidden');
+      x.activePopover = null;
+    }
   },
 });
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  console.log("Window Clicked");
-  console.log(event.target);
-  if (event.target == overlay) {
-    console.log("Remove popover");
-    document.body.classList.remove('blur');
-    console.log(currentSpeaker);
-    document.getElementById(currentSpeaker).style.display = "none";
-  }
-}
